f2e71471174ab81fe6a5cc6964cef7bc93e95b78
madibah <madibah@gilmour.org>
2016-11-18 09:45:13 -0500

    we just created a new method turn modifier method with Mr. addileeta

--- student.py
+++ student.py
@@ -143,7 +143,7 @@ class GoPiggy(pigo.Pigo):
     def setSpeed(self, left, right):
         print("left speed: " + str(left))
         print("right speed: " + str(right))
-        set_left_speed(int((left))
+        set_left_speed(int(left))
         set_right_speed(int(right))
         time.sleep(.05)
 
